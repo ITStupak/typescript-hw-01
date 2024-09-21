797c476c992fc73a4cbcfcd423ec523fb84d88de
Sergey Stupak <itstupak@gmail.com>
2024-09-21 11:24:17 +0200

    finished ts-hw-01

--- src/basic/4.ts
+++ src/basic/4.ts
@@ -3,18 +3,15 @@
 function showMessage(message) {
   console.log(message);
 }
-
 function calc(num1, num2) {
   return num1 + num2;
 }
-
 function customError() {
   throw new Error('Error');
 }
 
 // Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
  */
-
 
 function showMessage(message: string):void {
   console.log(message);
